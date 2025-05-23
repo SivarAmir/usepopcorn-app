e3529a590702c7f0bb69e5780c8959bc1e7168f3
Sivar Amir <109675821+SivarAmir@users.noreply.github.com>
2025-05-24 00:58:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,10 +51,10 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const APIKEY = "93c765ec";
+const APIKEY = "b5b87720";
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
